0801a68df8090b241e43ac27a629087b5f200fd0
Gabriele Di giampietro <116716975+gabryx2z@users.noreply.github.com>
2023-03-05 16:43:38 +0100

    Update Registro_entrata_uscita.py

--- Registro_entrata_uscita.py
+++ Registro_entrata_uscita.py
@@ -7,14 +7,14 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 # Definisci il nome del file in cui registrare gli accessi e le uscite
 file_name = "accessi_uscite.docx"
 
-# Verifica se il file esiste gi‡. Se sÏ, apri il documento esistente, altrimenti crea un nuovo documento
+# Verifica se il file esiste gi√†. Se s√¨, apri il documento esistente, altrimenti crea un nuovo documento
 if os.path.exists(file_name):
     document = Document(file_name)
 else:
     document = Document()
 
-# Chiedi all'utente se l'azione Ë un'entrata o un'uscita
-azione = input("L'azione Ë un'entrata o un'uscita? ")
+# Chiedi all'utente se l'azione √® un'entrata o un'uscita
+azione = input("L'azione √® un'entrata o un'uscita? ")
 
 # Ottieni la data e l'ora correnti
 now = datetime.datetime.now()
@@ -38,3 +38,9 @@ document.save(file_name)
 print("Accessi e uscite registrati:")
 for paragraph in document.paragraphs:
     print(paragraph.text)
+#Il programma utilizza la libreria datetime per ottenere la data e l'ora correnti, la libreria os
+#per verificare se il file esiste gi√†, e la libreria python-docx per creare il file Word e
+#aggiungere i paragrafi. Il programma chiede all'utente se l'azione √® un'entrata o un'uscita,
+#registra la data e l'ora correnti insieme all'azione in un nuovo paragrafo nel file Word, e alla
+#fine stampa il contenuto del documento.
+    
